59149fd653e292b75f8639feeee81ce33e88c451
Benny Wong <benny@bwong.net>
2015-01-25 17:14:35 -0500

    Initial commit

---
+++ Rakefile
@@ -0,0 +1,17 @@
+task :fuck do
+  sh "aws --profile #{ENV["AWS_PROFILE"]} s3 sync --exclude README.md --exclude \".git/*\" --exclude Rakefile --acl public-read . s3://fuckinggodateformat.com/"
+end
+
+task :flip do
+  # BW: Hacky as well. YOLO amirite?
+  str = File.read("index.html")
+  str.gsub(/Fucking Go/, "Flipping Go")
+  str.gsub(/fuckingblock/, "goshdarnblock")
+  str.gsub(/Fucking Block/, "Gosh Darn Block")
+  str.gsub(/Not a fan of profanity.*also works\./, "For the more NSFW site, <a href='http://fuckingblocksyntax.com/'>here</a>")
+
+  File.write("index.html", str)
+
+  sh "aws --profile #{ENV["AWS_PROFILE"]} s3 sync --exclude README.md --exclude \".git/*\" --exclude Rakefile --acl public-read . s3://flippinggodateformat.com/"
+  sh "git checkout ."
+end
---
+++ fgdf.js
@@ -0,0 +1,72 @@
+$(function() {
+  var mapping = {
+    "%a": "Mon",
+    "%A": "Monday",
+    "%b": "Jan",
+    "%B": "January",
+    "%d": "02",
+    "%e": "2",
+    "%m": "01",
+    "%y": "06",
+    "%Y": "2006",
+    "%H": "15",
+    "%I": "03",
+    "%l": "3",
+    "%M": "04",
+    "%P": "pm",
+    "%p": "PM",
+    "%S": "05",
+    "%Z": "MST",
+    "%%": "%",
+
+    // Codes that Go doesn't support
+    "%j": "%j",
+    "%U": "%U",
+    "%W": "%W",
+    "%w": "%w",
+    "%x": "%x",
+    "%X": "%X",
+    "%c": "%c"
+  }
+
+  function strftime(str) {
+    for (var c in mapping) {
+      var f = mapping[c];
+      str = replaceAll(str, c, f);
+    }
+
+    return str;
+  }
+
+  function replaceAll(str, find, replace) {
+    return str.replace(new RegExp(find, 'g'), replace);
+  }
+
+  function update() {
+    var input = $("#strftime").val();
+    var output = strftime(input);
+
+    $("#output").val(output);
+  }
+
+  function appendCode() {
+    var $inputEl = $("#strftime");
+    var code = $(this).text();
+
+    $inputEl.val($inputEl.val() + " " + code);
+    $inputEl.trigger("update");
+  }
+
+  function highlight() {
+    this.setSelectionRange(0, this.value.length);
+  }
+
+  $("#strftime").keyup(update);
+  $("#strftime").on("update", update);
+
+  $("#output").on("click", highlight);
+
+  $("#definitions dt").click(appendCode);
+
+  update()
+});
